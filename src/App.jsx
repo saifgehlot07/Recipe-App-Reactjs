cc0b089819607387e540b3a76746ce295cf3db03
saifgehlot07 <gehlotsaif07@gmail.com>
2023-01-08 00:00:44 +0530

    api fetched

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Pages from "./pages/Pages";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Pages />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Veggie.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Veggie = () => {
+  return <div>Veggie</div>;
+};
+
+export default Veggie;
